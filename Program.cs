3aae1ba956fe27af1c5a5858338e09cde4c17e0f
Douglas Paz <linodouglas321@gmail.com>
2024-10-06 11:52:45 -0300

    Adicionei método defender

--- Program.cs
+++ Program.cs
@@ -1,9 +1,15 @@
 ﻿using Escrevendo_as_Classes_de_Um_Jogo.models;
 
-Console.Write("Digite o nome do seu herói: ");
-string nome = Console.ReadLine();
+// Variável para verificar se o usuário deseja continuar no programa.
+string continuar = "";
 
-Console.Write(@"
+// Looping para inserir o usuário no programa.
+do
+{
+    Console.Write("Digite o nome do seu herói: ");
+    string nome = Console.ReadLine();
+
+    Console.Write(@"
 ============
 |Mago      |
 |Guerreiro |
@@ -11,7 +17,29 @@ Console.Write(@"
 |Ninja     |
 ============
 Digite o tipo do seu herói: ");
-string tipo = Console.ReadLine();
+    string tipo = Console.ReadLine();
+ 
+    Console.Write($"{nome} irá atacar ou defender? [A/D] ");
+    string ataqueOuDefesa = Console.ReadLine();
 
-Heroi h = new Heroi(nome, tipo);
-h.Atacar();
+    if (ataqueOuDefesa == "")
+    {
+        Console.WriteLine("Todas as informações devem ser preenchidas.");
+    }
+    else if (ataqueOuDefesa == "A")
+    {
+        // Instância da classe Heroi caso o usuário deseje atacar.
+        Heroi h = new Heroi(nome, tipo);
+        h.Atacar();
+    }
+    else
+    {
+        // Instância da classe Heroi caso o usuário deseje defender.
+        Heroi h = new Heroi(nome, tipo);
+        h.Defender();
+    }
+
+    Console.WriteLine("Deseja continuar? [S/N]");
+    continuar = Console.ReadLine();
+    Console.Clear();
+} while (continuar != "N");
--- models/Heroi.cs
+++ models/Heroi.cs
@@ -7,15 +7,48 @@ namespace Escrevendo_as_Classes_de_Um_Jogo.models
 {
     public class Heroi
     {
+        // Construtores para as propriedades Nome e Tipo.
         public Heroi(string nome, string tipo)
         {
             Nome = nome;
             Tipo = tipo;
         }
-        
-        public string Nome { get; set; }
-        public string Tipo { get; set; }
 
+        // Propriedades da classe Heroi.
+        string _nome;
+        public string Nome 
+        {
+            get => _nome;
+            
+            set
+            {
+                // Validação para o nome não ser vazio. Caso o nome seja vazio uma exceção será disparada.
+                if (value == "")
+                {
+                    throw new ArgumentException("Todas as informações devem ser preenchidas.");
+                }
+
+                _nome = value;
+            }
+        }
+        string _tipo;
+        public string Tipo 
+        {
+            get => _tipo;
+            
+            set
+            {
+                // Validação para o tipo não ser vazio. Caso o tipo seja vazio uma exceção será disparada.
+                if (value == "")
+                {
+                    throw new ArgumentException("Todas as informações devem ser preenchidas.");
+                }
+
+                _tipo = value;
+            }
+        }
+
+        // Métodos da classe Heroi.
         public void Atacar()
         {
             string ataque = "";
@@ -34,11 +67,59 @@ namespace Escrevendo_as_Classes_de_Um_Jogo.models
             }
             else
             {
-                ataque = "shuriken";
+                if (Nome == "Naruto")
+                {
+                    ataque = "rasengan";
+                }
+                else if (Nome == "Sasuke")
+                {
+                    ataque = "chidori";
+                }
+                else
+                {
+                    ataque = "estilo fogo";
+                }
             }
 
             Console.WriteLine();
             Console.WriteLine($"O {Nome} atacou usando -{ataque}-");
+            Console.WriteLine();
+        }
+
+        public void Defender()
+        {
+            string defesa = "";
+
+            if (Tipo == "Mago")
+            {
+                defesa = "feitiço de barreira";
+            }
+            else if (Tipo == "Guerreiro")
+            {
+                defesa = "escudo";
+            }
+            else if (Tipo == "Monge")
+            {
+                defesa = "bloqueio";
+            }
+            else
+            {
+                if (Nome == "Naruto")
+                {
+                    defesa = "clone das sombras";
+                }
+                else if (Nome == "Sasuke")
+                {
+                    defesa = "susano'o";
+                }
+                else
+                {
+                    defesa = "estilo terra";
+                }
+            }
+
+            Console.WriteLine();
+            Console.WriteLine($"O {Nome} defendeu usando -{defesa}-");
             Console.WriteLine();
         }
     }
